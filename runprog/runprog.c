c3608c98163bde119d1383fff0f2ad8a9c7246cd
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2026-02-09 17:05:36 +0100

    Use of exit

--- runprog/runprog.c
+++ runprog/runprog.c
@@ -10,32 +10,32 @@ void print_args(int argc, char *argv[]) {
   }
 }
 
-int run_program(int argc, char *argv[]) {
+void run_program(int argc, char *argv[]) {
   if (argc < 2) {
     fprintf(stderr, "Error: No program specified.\n");
     fprintf(stderr, "Usage: %s <program> [args...]\n", argv[0]);
-    return EXIT_FAILURE;
+    exit(EXIT_FAILURE);
   }
 
   pid_t pid = fork();
   switch (pid) {
   case -1:
     perror("fork");
-    return EXIT_FAILURE;
+    exit(EXIT_FAILURE);
   case 0: // Child process
   {
     execvp(argv[1], &argv[1]);
     // If execvp returns, it means there was an error
     perror("execvp");
-    return EXIT_FAILURE;
+    exit(EXIT_FAILURE);
   }
   default:
     printf("Parent process\n");
   }
-  return EXIT_SUCCESS;
 }
 
 int main(int argc, char *argv[]) {
   print_args(argc, argv);
-  return run_program(argc, argv);
+  run_program(argc, argv);
+  return 0;
 }

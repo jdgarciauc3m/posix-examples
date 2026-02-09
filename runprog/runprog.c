d033c73a73bc014c9de9bab569ebac62e2b968db
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2026-02-09 16:53:26 +0100

    Added error reporting

--- runprog/runprog.c
+++ runprog/runprog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -10,32 +11,32 @@ void print_args(int argc, char *argv[]) {
   }
 }
 
-void run_program(int argc, char *argv[]) {
+int run_program(int argc, char *argv[]) {
   if (argc < 2) {
     fprintf(stderr, "Error: No program specified.\n");
     fprintf(stderr, "Usage: %s <program> [args...]\n", argv[0]);
-    return;
+    return EXIT_FAILURE;
   }
 
   pid_t pid = fork();
   switch (pid) {
   case -1:
     perror("fork");
-    return;
+    return EXIT_FAILURE;
   case 0: // Child process
   {
     execvp(argv[1], &argv[1]);
     // If execvp returns, it means there was an error
     perror("execvp");
-    break;
+    return EXIT_FAILURE;
   }
   default:
     printf("Parent process\n");
   }
+  return EXIT_SUCCESS;
 }
 
 int main(int argc, char *argv[]) {
   print_args(argc, argv);
-  run_program(argc, argv);
-  return 0;
+  return run_program(argc, argv);
 }
